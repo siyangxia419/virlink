68d3e6f71d28340c4ae0a04f0188214380f1bc74
siyangxia419 <66046149+siyangxia419@users.noreply.github.com>
2021-07-01 22:22:10 -0400

    Description of the dataset "peptide_z.RData"

---
+++ R/peptide_z.R
@@ -0,0 +1,18 @@
+#' An example dataset of z-scores from VirScan analysis
+#'
+#' The dataset contains 50 peptides (column) and 12 samples (row) from the VirScan peptide library
+#'
+#' @docType data
+#'
+#' @usage data(peptide_z)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @source VirScan library
+#'
+#' @examples
+#' data(peptide_z)
+#' head(peptide_z)
+"peptide_z"
