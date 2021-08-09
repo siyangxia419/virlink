5f70e2c4ee4afcfdbf5f5d1b9013dc84bca248cb
siyangxia419 <66046149+siyangxia419@users.noreply.github.com>
2021-08-09 19:01:48 -0400

    Update function to improve handling of statistical tests and calculating coefficients

--- R/peptide_cooccurrence.R
+++ R/peptide_cooccurrence.R
@@ -1,54 +1,117 @@
 #' Summarize the co-occurrence of two peptides
 #'
-#' calculate a contingency table of two peptides' presence in multiple individuals and
-#' perform independency test and phi correlation coefficient
+#' calculate a contingency table of two peptides' presence in multiple individuals
+#' and perform independency test and cooccurrence coefficients
 #'
-#' @param x1 a logical vector of peptide 1's presence in multiple samples
-#' @param x2 a logical vector of peptide 2's presence in multiple samples
-#' @param test_method method to perform statistical test, options: "fisher" or "chisq" or "none"
-#' @param correlation whether phi correlation is calculated
+#' @param x a logical vector of peptide 1's presence in multiple samples
+#' @param y a logical vector of peptide 2's presence in multiple samples
+#' @param coefficient the cooccurrence coefficient to compute.
+#'     Options: "phi" (phi correlation coefficient), "prop" (mean proportion of
+#'     both peptide being enriched given one of them is enriched), "both"
+#'     (both "phi" and "prop").
+#'     Default: "phi"
+#' @param perform_test a logical indicator of whether statistical tests
+#'     should be performed for each comparison.
+#'     The test method is specified by \code{test_method}.
+#'     Default: \code{FALSE}
+#' @param test_method method to perform statistical test.
+#'     Options: "fisher" (Fisher's exact test) or "chisq" (Pearson's Chi-squared test).
+#'     Default: "fisher"
 #'
-#' @return a vector that contains 4-7 elements (depending on whether to perform test and correlation):
-#'   TT: frequency of both peptide positive
-#'   TF: frequency of peptide 1 positive and peptide 2 negative
-#'   FT: frequency of peptide 1 genative and peptide 2 position
-#'   FF: frequency of both peptide negative
-#'   stat: key statistics from the selected test (odds ratio for Fisher's exact test and chi-squared for chi-squared test)
-#'   p: p value from the selected test
-#'   phi: phi correlation coefficient (calculated by Pearson correlation)
+#' @return if \code{coefficient} = "phi" and \code{perform_test} = \code{FALSE},
+#'     return a single number, i.e., the phi coefficient. Otherwise, return
+#'     a vector that contains at least these four elements:
+#'     TT: frequency of both peptide positive;
+#'     TF: frequency of peptide 1 positive and peptide 2 negative;
+#'     FT: frequency of peptide 1 genative and peptide 2 position;
+#'     FF: frequency of both peptide negative.
+#'     Depending on \code{coefficient}, the vector also contains phi coefficient
+#'     and/or the mean proportion of cooccurrence. If \code{perform_test} is
+#'     \code{TRUE}, the vector also contains the key statistic ("oddsr" - odds ratio,
+#'     if using the Fisher's exact test, or "xsq" - chi-squared, if using the Chi-squared
+#'     test), and the p-value.
 #'
 #' @author Siyang Xia \email{sxia@@hsph.harvard.edu}
-#' @seealso \code{\link{peptide_pairwise_coocurrence}}
+#' @seealso \code{\link{peptide_pairwise_correlation}},
+#'     \code{\link[stats]{fisher.test}},
+#'     \code{\link[stats]{chisq.test}}
 #' @keywords peptide co-occurrence
 #'
 #' @examples
 #' data(peptide_z)
 #' peptide_bi <- as.data.frame((peptide_z > 3.5) * 1)
-#' cooccur <- peptide_cooccurrence(x1 = peptide_bi[, 1], x2 = peptide_bi[, 2])
-#' print(cooccur)
+#' peptide_cooccurrence(x = peptide_bi[, 1], y = peptide_bi[, 2],
+#'                      coefficient = "both",
+#'                      perform_test = TRUE,
+#'                      test_method = "fisher")
+#' peptide_cooccurrence(x = peptide_bi[, 1], y = peptide_bi[, 2],
+#'                      coefficient = "phi",
+#'                      perform_test = TRUE,
+#'                      test_method = "chisq")
+#' peptide_cooccurrence(x = peptide_bi[, 1], y = peptide_bi[, 2],
+#'                      coefficient = "prop",
+#'                      perform_test = FALSE)
+#' peptide_cooccurrence(x = peptide_bi[, 1], y = peptide_bi[, 2],
+#'                      coefficient = "phi",
+#'                      perform_test = FALSE)
 #'
 #' @export
 #'
-peptide_cooccurrence <- function(x1, x2,
-                                 test_method = "fisher",
-                                 correlation = TRUE){
+peptide_cooccurrence <- function(x, y,
+                                 coefficient = "phi",
+                                 perform_test = FALSE,
+                                 test_method = "fisher"){
 
-  v <- c(TT = sum(x1 & x2),
-         TF = sum(x1 & (!x2)),
-         FT = sum((!x1) & x2),
-         FF = sum((!x1) & (!x2)))
+  if(coefficient == "phi" & perform_test == FALSE){
 
-  # Test of association
-  if(test_method == "chisq"){         # Chi-square test
-    suppressWarnings(chi_t <- chisq.test(matrix(v, nrow = 2)))
-    v <- c(v, xsq = ifelse(is.null(chi_t$statistic), NA, chi_t$statistic), p = chi_t$p.value)
-  }else if(test_method == "fisher"){  # Fisher's exact test
-    suppressWarnings(fisher_t <- fisher.test(matrix(v, nrow = 2)))
-    v <- c(v, oddsr = ifelse(is.null(fisher_t$estimate), NA, fisher_t$estimate), p = fisher_t$p.value)
+    return(suppressWarnings(cor(x, y, method = "pearson")))
+
+  }else{
+
+    tr <- c(TT = sum(x & y),
+            TF = sum(x & (!y)),
+            FT = sum((!x) & y),
+            FF = sum((!x) & (!y)))
+
+    # Add test results if desired:
+    if(perform_test == TRUE & test_method == "fisher"){
+      suppressWarnings(fisher_t <- fisher.test(matrix(tr, nrow = 2)))
+      tr <- c(tr,
+              oddsr = ifelse(is.null(fisher_t$estimate), NA, fisher_t$estimate),
+              p = fisher_t$p.value)
+    }
+    if(perform_test == TRUE & test_method == "chisq"){
+      suppressWarnings(chi_t <- chisq.test(matrix(tr, nrow = 2)))
+      tr <- c(tr,
+              xsq = ifelse(is.null(chi_t$statistic), NA, chi_t$statistic),
+              p = chi_t$p.value)
+    }
+
+    # Add cooccurrence coefficient if desired
+    if(coefficient == "phi"){
+      tr <- c(tr, phi = suppressWarnings(cor(x, y, method = "pearson")))
+    }
+    if(coefficient == "prop"){
+      TT_NT <- ifelse(test = (tr["TT"] + tr["FT"]) > 0,
+                      yes = tr["TT"] / (tr["TT"] + tr["FT"]),
+                      no = 0)
+      TT_TN <- ifelse(test = (tr["TT"] + tr["TF"]) > 0,
+                      yes = tr["TT"] / (tr["TT"] + tr["TF"]),
+                      no = 0)
+      tr <- c(tr, mean_prop = mean(TT_NT, TT_TN))
+    }
+    if(coefficient == "both"){
+      TT_NT <- ifelse(test = (tr["TT"] + tr["FT"]) > 0,
+                      yes = tr["TT"] / (tr["TT"] + tr["FT"]),
+                      no = 0)
+      TT_TN <- ifelse(test = (tr["TT"] + tr["TF"]) > 0,
+                      yes = tr["TT"] / (tr["TT"] + tr["TF"]),
+                      no = 0)
+      tr <- c(tr,
+              phi = suppressWarnings(cor(x, y, method = "pearson")),
+              mean_prop = mean(TT_NT, TT_TN))
+    }
+
+    return(tr)
   }
-
-  # Phi coefficient: calculated by Pearson correlation, equals to sqrt(chi-square / n)
-  if(correlation) v <- c(v, phi = suppressWarnings(cor(x1, x2, method = "pearson")))
-
-  return(v)
 }
--- tests/testthat/test_peptide_cooccurrence.R
+++ tests/testthat/test_peptide_cooccurrence.R
@@ -3,23 +3,33 @@ library(virlink)
 data("peptide_z")
 peptide_bi <- as.data.frame((peptide_z > 3.5) * 1)
 
-test_that("output is a numeric vector", {
-  test_result1 <- peptide_cooccurrence(x1 = peptide_bi[, 1],
-                                       x2 = peptide_bi[, 2],
-                                       test_method = "fisher",
-                                       correlation = TRUE)
+test_that("output is a number if only phi coefficient is desired", {
+  test_result1 <- peptide_cooccurrence(x = peptide_bi[, 1],
+                                       y = peptide_bi[, 2],
+                                       coefficient = "phi",
+                                       perform_test = FALSE)
+  expect_type(test_result1, type = "double")
+})
+#> Test passed
 
-  expect_equal(length(test_result1), 7)
-  expect_equal(names(test_result1), c("TT", "TF", "FT", "FF", "oddsr", "p", "phi"))
-  expect_equal(sum(test_result1[c("TT", "TF", "FT", "FF")]), length(peptide_bi[, 1]))
+test_that("output is a numeric vector, otherwise", {
+  test_result2 <- peptide_cooccurrence(x = peptide_bi[, 1],
+                                       y = peptide_bi[, 2],
+                                       coefficient = "both",
+                                       perform_test = TRUE,
+                                       test_method = "fisher")
 
-  test_result2 <- peptide_cooccurrence(x1 = peptide_bi[, 1],
-                                       x2 = peptide_bi[, 2],
-                                       test_method = "chisq",
-                                       correlation = FALSE)
+  expect_type(test_result2, type = "double")
+  expect_vector(test_result2)
+  expect_equal(length(test_result2), 8)
+  expect_equal(names(test_result2),
+               c("TT", "TF", "FT", "FF", "oddsr", "p", "phi", "mean_prop"))
+  expect_equal(sum(test_result2[c("TT", "TF", "FT", "FF")]),
+               length(peptide_bi[, 1]))
+})
+#> Test passed
 
-  expect_equal(length(test_result2), 6)
-  expect_equal(names(test_result2), c("TT", "TF", "FT", "FF", "xsq", "p"))
-  expect_equal(sum(test_result2[c("TT", "TF", "FT", "FF")]), length(peptide_bi[, 2]))
+test_that("invalid input causes error", {
+  expect_error(peptide_cooccurrence(x = peptide_bi[, 1], y = numeric(0)))
 })
 #> Test passed
