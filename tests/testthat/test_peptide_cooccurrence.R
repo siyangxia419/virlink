bbc275734c4a998973adbc350197fb1d2b716f2a
siyangxia419 <66046149+siyangxia419@users.noreply.github.com>
2021-12-01 14:58:20 -0500

    update the test to accommodate the changes in the function

--- tests/testthat/test_peptide_cooccurrence.R
+++ tests/testthat/test_peptide_cooccurrence.R
@@ -15,15 +15,15 @@ test_that("output is a number if only phi coefficient is desired", {
 test_that("output is a numeric vector, otherwise", {
   test_result2 <- peptide_cooccurrence(x = peptide_bi[, 1],
                                        y = peptide_bi[, 2],
-                                       coefficient = "both",
+                                       coefficient = c("jaccard", "phi", "prop"),
                                        perform_test = TRUE,
                                        test_method = "fisher")
 
   expect_type(test_result2, type = "double")
   expect_vector(test_result2)
-  expect_equal(length(test_result2), 8)
+  expect_equal(length(test_result2), 9)
   expect_equal(names(test_result2),
-               c("TT", "TF", "FT", "FF", "oddsr", "p", "phi", "mean_prop"))
+               c("TT", "TF", "FT", "FF", "oddsr", "p", "jaccard", "phi", "mean_prop"))
   expect_equal(sum(test_result2[c("TT", "TF", "FT", "FF")]),
                length(peptide_bi[, 1]))
 })
